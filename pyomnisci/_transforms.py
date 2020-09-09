c096b4135b848f8414a199e69bf0bf51bc7a2bd4
jp-harvey <jpharvey@cloudquarterback.com>
2020-09-09 13:50:45 -0700

    Update change_dashboard_sources to work with tabbed or regular dashboards

--- pyomnisci/_transforms.py
+++ pyomnisci/_transforms.py
@@ -24,37 +24,51 @@ def change_dashboard_sources(dashboard, remap):
     # Load our dashboard state into a python dictionary
     ds = json.loads(b64decode(dashboard.dashboard_state).decode())
 
+    # If it's a 5.4 tabbed dashboard, iterate through the tabs
+    # otherwise conver the dashboard state as-is
+    if ds.get('tabs', None):
+        for tabname, tabdef in ds.get('tabs').items():
+            ds['tabs'][tabname] = _table_remap(tabdef, remap)
+    else:
+        ds = _table_remap(ds, remap)
+
+    # Convert our new dashboard state to a python object
+    dashboard.dashboard_state = b64encode(json.dumps(ds).encode()).decode()
+    dashboard.dashboard_metadata = json.dumps(dm)
+    return dashboard
+
+def _table_remap(tab, remap):
     # Remap items in our old dashboard state to the new table name
     for old_table, defs in remap.items():
         new_table = defs.get('name', {}) or old_table
-        if ds['dashboard']['table'] == old_table:
-            ds['dashboard']['table'] = new_table
+        if tab['dashboard']['table'] == old_table:
+            tab['dashboard']['table'] = new_table
 
         # Change the name of the dashboard or use the old one
-        ds['dashboard']['title'] = (
-            defs.get('title', {}) or ds['dashboard']['title']
+        tab['dashboard']['title'] = (
+            defs.get('title', {}) or tab['dashboard']['title']
         )
 
         # Remap our datasources keys
-        for key, val in ds['dashboard']['dataSources'].items():
+        for key, val in tab['dashboard']['dataSources'].items():
             for col in val['columnMetadata']:
                 if col['table'] == old_table:
                     col['table'] = new_table
 
         # Remap our charts to the new table
-        for c, val in ds['charts'].items():
+        for c, val in tab['charts'].items():
             if val.get('dataSource', None):
-                if ds['charts'][c]['dataSource'] == old_table:
-                    ds['charts'][c]['dataSource'] = new_table
+                if tab['charts'][c]['dataSource'] == old_table:
+                    tab['charts'][c]['dataSource'] = new_table
 
             # Remap Our Dimensions to the new table
             i = 0
             for dim in val.get('dimensions', []):
                 if dim.get('table', {}) == old_table:
-                    ds['charts'][c]['dimensions'][i]['table'] = new_table
+                    tab['charts'][c]['dimensions'][i]['table'] = new_table
                 if dim.get('selector', {}).get('table') == old_table:
                     (
-                        ds['charts'][c]['dimensions'][i]['selector']['table']
+                        tab['charts'][c]['dimensions'][i]['selector']['table']
                     ) = new_table
                 i += 1
 
@@ -62,7 +76,7 @@ def change_dashboard_sources(dashboard, remap):
             i = 0
             for m in val.get('measures', []):
                 if m.get('table', None) == old_table:
-                    ds['charts'][c]['measures'][i]['table'] = new_table
+                    tab['charts'][c]['measures'][i]['table'] = new_table
                 i += 1
 
             # Remap Our Layers to the new table
@@ -70,21 +84,18 @@ def change_dashboard_sources(dashboard, remap):
             for layer in val.get('layers', []):
                 im = 0
                 if layer.get('dataSource', {}) == old_table:
-                    ds['charts'][c]['layers'][il]['dataSource'] = new_table
+                    tab['charts'][c]['layers'][il]['dataSource'] = new_table
                 for measure in layer.get('measures', []):
                     if measure.get('table', None) == old_table:
                         (
-                            ds['charts'][c]['layers'][il]['measures'][im][
+                            tab['charts'][c]['layers'][il]['measures'][im][
                                 'table'
                             ]
                         ) = new_table
                     im += 1
                 il += 1
-        (ds['dashboard']['dataSources'][new_table]) = ds['dashboard'][
-            'dataSources'
-        ].pop(old_table)
-
-    # Convert our new dashboard state to a python object
-    dashboard.dashboard_state = b64encode(json.dumps(ds).encode()).decode()
-    dashboard.dashboard_metadata = json.dumps(dm)
-    return dashboard
+        if tab['dashboard']['dataSources'].get(old_table, None):
+            (tab['dashboard']['dataSources'][new_table]) = tab['dashboard'][
+                'dataSources'
+            ].pop(old_table)
+        return tab
